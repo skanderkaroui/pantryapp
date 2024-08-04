fecf14be9166a0b772572e48145482d23178bab8
skanderkaroui <skander.karoui@gmail.com>
2024-08-04 22:08:18 +0100

    pantrypage'

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import LandingPage from "@/landing page/LandingPage";
+import PantryPage from "./PantryPage";
 
 export default function Home() {
-  return <LandingPage />;
+  return <PantryPage />;
 }
