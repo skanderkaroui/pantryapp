52cfdd38e58b32eba90eb11a6ed94946ec4b8fa7
skanderkaroui <skander.karoui@gmail.com>
2024-08-04 23:32:02 +0100

    rm footer

--- landing page/LandingPage.js
+++ landing page/LandingPage.js
@@ -30,27 +30,7 @@ function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
         position: "fixed",
         bottom: 24,
       }}
-    >
-      <ToggleButtonGroup
-        color="primary"
-        exclusive
-        value={showCustomTheme}
-        onChange={toggleCustomTheme}
-        aria-label="Platform"
-        sx={{
-          backgroundColor: "background.default",
-          "& .Mui-selected": {
-            pointerEvents: "none",
-          },
-        }}
-      >
-        <ToggleButton value>
-          <AutoAwesomeRoundedIcon sx={{ fontSize: "20px", mr: 1 }} />
-          Custom theme
-        </ToggleButton>
-        <ToggleButton value={false}>Material Design 2</ToggleButton>
-      </ToggleButtonGroup>
-    </Box>
+    ></Box>
   );
 }
 
