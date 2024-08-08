bef574cf76409bc27b1b93692751ae867d040505
skanderkaroui <skander.karoui@gmail.com>
2024-08-08 11:27:14 +0100

    project metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import PantryPage from "./PantryPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Pantry Tracker - My App",
+  description: "Manage and track your pantry items effectively.",
+};
 
 export default function Home() {
   return <PantryPage />;
